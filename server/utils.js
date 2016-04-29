b69d912d968451f677e6e8f5e64c4e7c1a80ca33
Artem Tymchenko <ar.timchencko@gmail.com>
2016-04-29 18:45:31 +0300

    Change share message

--- server/utils.js
+++ server/utils.js
@@ -30,19 +30,36 @@ export function getMetaDataFromState({ route, state, lang = 'en', params = {}, q
     }
 
     if (route === '/result/:id/:userId' && state.currentActivation.activation) {
-        const sharePhrases = {
+        let sharePhrases = {
             ru: 'Я сдал тест "{name}" на {score}%',
             uk: 'Я склав тест "{name}" на {score}%',
             en: 'I have passed test "{name}" and gained {score}%'
         };
 
-        const { name, pictureURL, message, userQuizSession } = state.currentActivation.activation;
+        const { name, pictureURL, message, userQuizSession, assessmentSystem } = state.currentActivation.activation;
+
+        let title = strformat(sharePhrases[lang], { name, score: userQuizSession.score });
+        let greetingDescription = '';
+
+        if (assessmentSystem.length) {
+            const greeting = _getGreeting(assessmentSystem, userQuizSession.score);
+
+            sharePhrases = {
+                ru: 'Я сдал тест "{name}" на {score}%. Мой результат: "{greeting}"',
+                uk: 'Я склав тест "{name}" на {score}%. Мій результат: "{greeting}"',
+                en: 'I have passed test "{name}" and gained {score}%. My result is: "{greeting}"'
+            };
+
+            title = strformat(sharePhrases[lang], { name, score: userQuizSession.score, greeting: greeting.phrase });
+            greetingDescription = greeting.description;
+        }
+
 
         return {
-            title       : strformat(sharePhrases[lang], { name, score: userQuizSession.score }),
+            title,
             siteName    : "It's quiz",
             image       : pictureURL ? pictureURL.replace('svg', 'png') : '',
-            description : message
+            description : greetingDescription || message
         };
     }
 
@@ -110,4 +127,15 @@ export function detectLocale(req) {
         RU: 'ru',
         TR: 'tr'
     }[country] || 'en';
+}
+
+function _getGreeting(assessmentSystem, score) {
+    for (let i = assessmentSystem.length - 1; i >= 0; i--) {
+        if (score >= assessmentSystem[i].grade) {
+            return {
+                phrase: assessmentSystem[i].phrase,
+                description: assessmentSystem[i].description || ''
+            };
+        }
+    }
 }
